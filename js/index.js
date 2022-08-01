a5b5656f15e71146fb93270235335c2773d2e9de
Kirill Lavrov <lkv@myttk.ru>
2022-08-01 15:05:21 +0700

    fix request

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-import { URL_API, token } from "./modules/const.js";
+import { API_KEY, REQUEST_URL } from "./modules/const.js";
 
 const getNews = async () => {
   try {
-    const response = await fetch(URL_API, {
+    const response = await fetch(REQUEST_URL, {
       headers: {
-        'x-api-key': `${token}`,
+        Authorization: API_KEY,
       },
     });
     const data = await response.json();
--- js/modules/const.js
+++ js/modules/const.js
@@ -1,2 +1,6 @@
-export const URL_API = `https://newsapi.org/v2/everything?q=keyword&apiKey=cd7d3eb315884c16bb59a54361900b33`;
-export const token = `cd7d3eb315884c16bb59a54361900b33`;
+export const PROXY_URL = "https://cors-anywhere.herokuapp.com/"
+export const API_KEY = "cd7d3eb315884c16bb59a54361900b33";
+export const RU = `ru`;
+export const URL = `${PROXY_URL}top-headlines?country=${RU}&apiKey=${API_KEY}`;
+export const REQUEST_URL = new Request(URL);
+
