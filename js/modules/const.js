6d0ac7674d01a576249501a4ab0b091989d16121
Kirill Lavrov <lkv@myttk.ru>
2022-08-01 14:38:55 +0700

    fix request

--- js/modules/const.js
+++ js/modules/const.js
@@ -1,2 +1,2 @@
-export const URL_API = `https://newsapi.org/v2/top-headlines?country=ru`;
+export const URL_API = `https://newsapi.org/v2/everything?q=keyword&apiKey=cd7d3eb315884c16bb59a54361900b33`;
 export const token = `cd7d3eb315884c16bb59a54361900b33`;
